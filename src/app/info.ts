d8d008212122a1f6729294847e93d2da94f61fee
G-2615 <gauravbhardwaj261@gmail.com>
2020-03-02 13:25:46 +0530

    first commit

---
+++ src/app/info.ts
@@ -0,0 +1,13 @@
+export class InfoModels
+{
+    firstName:string;
+    lastName:string;
+    age:number;
+    email:string;
+    phone:number;
+    city:string;
+    addr:string;
+    //city:string;
+    package:string;
+    trainpref:string;
+}
